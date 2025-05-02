08589e7840935ce1e96926de95028e546f6b982c
Gacik Sasa <gacik.mail@gmail.com>
2025-05-02 18:58:58 +0200

    Added project edit view

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use App\Models\TaskStatus;
 use App\Models\User;
+use App\Models\Department;
 use App\Traits\LogsUserActivity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
