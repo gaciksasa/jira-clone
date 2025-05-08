7c05e4b1e7c37da4b9cca1fe71524dd58862104c
Gacik Sasa <gacik.mail@gmail.com>
2025-05-08 20:24:04 +0200

    Added Bootstrap custom theme CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,13 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     
     <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.5/dist/litera/bootstrap.min.css" rel="stylesheet">
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.5/dist/spacelab/bootstrap.min.css" rel="stylesheet">-->
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <style>
         .bg-admin {
-            background-color:rgb(188, 215, 255);
+            background-color: lightgray;
         }
 
         /* Styles for task cards */
