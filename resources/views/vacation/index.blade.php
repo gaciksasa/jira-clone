4473fabea6ab795d8e3757a235acaddc4c9d43df
Gacik Sasa <gacik.mail@gmail.com>
2025-05-10 21:09:29 +0200

    removed close btn from modal form

--- resources/views/vacation/index.blade.php
+++ resources/views/vacation/index.blade.php
@@ -321,7 +321,6 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="requestVacationModalLabel">Request Days Off</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form method="POST" action="{{ route('vacation.store') }}">
                 @csrf
